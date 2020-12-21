2ec7ff8b0596d4e3333537a949958121dc24b572
Liski <liski.aleksi@gmail.com>
2020-12-21 13:45:07 +0200

    Formatting project

---
+++ src/dtf/datafile/DataFile.java
@@ -0,0 +1,15 @@
+package dtf.datafile;
+
+import java.util.ArrayList;
+
+public class DataFile {
+
+	public static ArrayList<DataFile> datafiles = new ArrayList<DataFile>();
+	
+	private String name;
+	
+	public DataFile() {
+		
+	}
+	
+}
